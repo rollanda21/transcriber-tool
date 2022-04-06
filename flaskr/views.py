92df6d534c0dfa60d2567caa45c1506da8450bff
rollanda21 <rollanda21@gmail.com>
2022-04-06 23:28:42 +0100

    Convert txt file into srt file working

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for, jsonify, request, redirect, flash
+from flask import Blueprint, render_template, url_for, jsonify, request, redirect, flash, send_file
 from werkzeug.utils import secure_filename
 import os
 from flask import current_app as app
@@ -8,6 +8,9 @@ import pydub
 from pydub import AudioSegment
 import math
 
+import re
+from datetime import datetime, timedelta
+
 views = Blueprint('views', __name__)
 
 #******* Global variables********
@@ -15,8 +18,7 @@ filename = ''
 chunks_counter = []
 text = []
 transcript = []
-
-
+srtout = ''
 
 @views.route('/')
 def home():
@@ -35,10 +37,10 @@ def services():
     return render_template("services.html")
 
 # Functions that check if an extension is valid and that uploads the file and redirects the user to the URL for the uploaded file:
-ALLOWED_EXTENSIONS = {'wav', 'mp3'}
-def allowed_file(filename):
+ALLOWED_AUDIO_EXTENSIONS = {'wav', 'mp3'}
+def allowed_audio_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_AUDIO_EXTENSIONS
     
 @views.route('/upload_audio', methods = ['GET', 'POST'])
 def upload_audio():
@@ -53,7 +55,7 @@ def upload_audio():
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
+        if file and allowed_audio_file(file.filename):
             global filename
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -64,8 +66,7 @@ def upload_audio():
 
 # Split audios in small chunks
 print('************************starting*************************')
-print('./flaskr/chunks/' + filename)
-
+#print(os.path.join(os.path.dirname(__file__), './flaskr/chunks/' + filename))
 class SplitWavAudio():
     def __init__(self):
         #self.folder = folder
@@ -172,8 +173,15 @@ def transcribe():
 
 
 # Generate subtitles
-@views.route('/upload_video', methods = ['GET', 'POST'])
-def upload_video():
+# Functions that check if an extension is valid and that uploads the file and redirects the user to the URL for the uploaded file:
+ALLOWED_TEXT_EXTENSIONS = {'txt'}
+def allowed_text_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_TEXT_EXTENSIONS
+
+@views.route('/upload_text', methods = ['GET', 'POST'])
+def upload_text():
+    print('uploading text file')
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -185,15 +193,65 @@ def upload_video():
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
-        if file and allowed_file(file.filename):
+        if file and allowed_text_file(file.filename):
             global filename
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('views.transcribe', name=filename))
-    return render_template('upload_video.html')
-@views.route('/subtitle')
+            return redirect(url_for('views.subtitle', name=filename))
+    return render_template('upload_text.html')
+
+@views.route('/subtitle', methods = ['GET', 'POST'])
 def subtitle():
-    return render_template("subtitle.html")
+    # initializing subtitle length in seconds
+    dursec = 3
+
+    # intializing .txt file locared in the same folder as this python script
+    inputtxt = './flaskr/chunks/' + filename
+    #subpath = os.path.join(os.path.dirname(__file__), inputtxt)
+    subtxt = open(inputtxt).read()
+
+    # splitting paragraphs into list items with regex
+    par = re.split('\n{2,}', subtxt)
+
+    # pulling number of paragraphs in a text doc
+    npar = len(par)
+
+    # initializing starting subtitle and subtitile duration
+    tdstart = timedelta(hours=0, seconds=-dursec)
+    tddur = timedelta(seconds=dursec)
+
+    # creating a list of timedeltas
+    tdlist = []
+    for i in range(npar+1):
+        tdstart = tdstart + tddur
+        tdlist.append(tdstart)
+
+    # combining created list into a string in accordance with .srt formatting
+    lcomb = []
+    for i in range(npar):
+        lcomb.append(str(i+1) + '\n' + str(tdlist[i]) + ',000 --> ' + str(
+            tdlist[i+1]) + ',000' + '\n' + par[i] + '\n')
+
+    # converting the list into a string with the delimiter '\n'
+    srtstring = '\n'.join(lcomb)
+
+    # adding '0' to single digit hours
+    pat = r'^(\d:)'
+    repl = '0\\1'
+    srtstring2 = re.sub(pat, repl, srtstring, 0, re.MULTILINE)
+
+    # writing the string to a new file
+    global srtout
+    srtout = './flaskr/chunks/' + filename.split('.')[0] + '.' + 'srt'
+    with open(srtout, 'w') as newfile:
+        newfile.write(srtstring2)
+
+    #send_file(path, as_attachment=True)
+
+    return render_template("subtitle.html", filename=filename)
+
+
+
     
 @views.route('/record')
 def record():
